70528acea530516eb3f84352cadc346ee02a3689
Hassam <101381873+Hassam-07@users.noreply.github.com>
2024-01-09 10:11:42 +0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'quiz-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'quiz-app';
+}
